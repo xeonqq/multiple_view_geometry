f1271b103583cc765e390852ad0036344e3bf44e
Qian <xeonqq@gmail.com>
2019-10-03 23:49:14 +0200

    Make LinearEquation support array operation

--- linear_equation.py
+++ linear_equation.py
@@ -1,11 +1,16 @@
 class LinearEquation(object):
     def __init__(self, coefficents):
-        # coefficents are in the order a ,b, c where ax + by + c = 0
+        # coefficents are in the order [a ,b, c] or of shape (3, N) where ax + by + c = 0
         self._coefficents = coefficents
 
     def solve_y(self, x):
-        a, b, c = self._coefficents
-        assert b != 0
+        if self._coefficents.ndim == 1:
+            a, b, c = self._coefficents
+            assert b != 0
+        else:
+            a = self._coefficents[:,:2]
+            b = self._coefficents[:,:2]
+            c = self._coefficents[:,:2]
         return (-c-x*a)/b
 
 
