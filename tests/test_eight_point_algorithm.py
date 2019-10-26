b2b54f84867eb0eab138c694e200b998e127c8b7
Qian <xeonqq@gmail.com>
2019-10-26 15:45:05 +0200

    Add test for reconstruct_translation_and_rotation

--- tests/test_eight_point_algorithm.py
+++ tests/test_eight_point_algorithm.py
@@ -4,7 +4,8 @@ import unittest
 import numpy as np
 
 from .scene_fixture import SceneFixture
-from multiple_view_geometry.algorithm import solve_essential_matrix, calculate_essential_matrix, reconstruct_translation_and_rotation_from_svd_of_essential_matrix
+from multiple_view_geometry.algorithm import solve_essential_matrix, calculate_essential_matrix, reconstruct_translation_and_rotation_from_svd_of_essential_matrix, structure_from_motion
+from multiple_view_geometry.homogeneous_matrix import HomogeneousMatrix
 
 class TestEightPointAlgorithm(SceneFixture):
     def test_eight_point_algorithm(self):
@@ -16,18 +17,24 @@ class TestEightPointAlgorithm(SceneFixture):
                 self._points_in_image_frame1, self._camera1)
         true_essential_matrix = calculate_essential_matrix(self._camera1, self._camera0)
 
-        #tf = self._camera1.get_transform_wrt(self._camera0)
-        #T = tf.translation
-        #R = tf.rotation
+        tf = self._camera1.get_transform_wrt(self._camera0)
 
         # there are two possible solutions, after we implement reconstuct stuction function, we can check
         # the plausible solution should have positive depth
-        # T1, R1, T2, R2 = reconstruct_translation_and_rotation_from_svd_of_essential_matrix(u, s, vh)
+        T1, R1, T2, R2 = reconstruct_translation_and_rotation_from_svd_of_essential_matrix(u, s, vh)
+        points_3d_in_camera1_frame, points_3d_scale = structure_from_motion( \
+                self._camera0.points_2d_to_homogeneous_coordinates(self._points_in_image_frame0), \
+                self._camera1.points_2d_to_homogeneous_coordinates(self._points_in_image_frame1), \
+                HomogeneousMatrix.create(T1, R1))
 
+        tranlsation_cam1_wrt_cam0, rotation_cam1_wrt_cam0 = (T1, R1) if (points_3d_scale is not None and (points_3d_scale > 0).all()) else (T2, R2)
         scale = 3./5.
         # the calculated essential matrix is the same as the true_essential_matrix by a scale
-        np.testing.assert_array_almost_equal(essential_matrix*(scale), true_essential_matrix)
+        np.testing.assert_array_almost_equal(essential_matrix*scale, true_essential_matrix)
 
+        # test the recovered translation and rotation is the same as the ground truth
+        np.testing.assert_array_almost_equal(tranlsation_cam1_wrt_cam0*scale, tf.translation)
+        np.testing.assert_array_almost_equal(rotation_cam1_wrt_cam0, tf.rotation)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_structure_from_motion.py
+++ tests/test_structure_from_motion.py
@@ -15,8 +15,9 @@ class TestStuctureFromMotion(SceneFixture):
         homogeneous_points_in_camera_frame1 = self._camera1.points_2d_to_homogeneous_coordinates(self._points_in_image_frame1)
 
         transform = self._camera1.get_transform_wrt(self._camera0)
-        world_points_in_camera1 = structure_from_motion(homogeneous_points_in_camera_frame0, homogeneous_points_in_camera_frame1, transform);
+        world_points_in_camera1, _ = structure_from_motion(homogeneous_points_in_camera_frame0, homogeneous_points_in_camera_frame1, transform);
         true_world_points_in_camera1 = self._camera1.world_frame_to_camera_frame(self._key_points_cube)
+
         np.testing.assert_array_almost_equal(world_points_in_camera1, true_world_points_in_camera1)
 
 if __name__ == '__main__':
