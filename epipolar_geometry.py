7be76e786e60113c7e9760be2dfe416787b63be1
Qian <xeonqq@gmail.com>
2019-12-07 16:08:15 +0100

    Format python code

--- epipolar_geometry.py
+++ epipolar_geometry.py
@@ -8,15 +8,16 @@ from multiple_view_geometry.camera_image_renderer import CameraImageRenderer
 from multiple_view_geometry.homogeneous_matrix import HomogeneousMatrix
 from multiple_view_geometry.transform_utils import create_rotation_mat_from_rpy
 
-if __name__ == '__main__':
-    camera0_extrinsic = HomogeneousMatrix.create([1.7,0.0,0.5], create_rotation_mat_from_rpy(-np.pi/2, 0, -np.pi/4))
-    camera0 = Camera('0', camera0_extrinsic)
-    camera1_extrinsic = HomogeneousMatrix.create([2.3,0.0,0.5], create_rotation_mat_from_rpy(-np.pi/2, 0.0, 0))
-    camera1 = Camera('1', camera1_extrinsic)
+if __name__ == "__main__":
+    camera0_extrinsic = HomogeneousMatrix.create(
+        [1.7, 0.0, 0.5], create_rotation_mat_from_rpy(-np.pi / 2, 0, -np.pi / 4)
+    )
+    camera0 = Camera("0", camera0_extrinsic)
+    camera1_extrinsic = HomogeneousMatrix.create([2.3, 0.0, 0.5], create_rotation_mat_from_rpy(-np.pi / 2, 0.0, 0))
+    camera1 = Camera("1", camera1_extrinsic)
 
-    cube = Cube((2,3,0), (2,2,2), resolution=1)
-    renderer = CameraImageRenderer({camera0: 'red', camera1: 'blue'}, show_image_frame=True, show_epipolar_lines=True)
+    cube = Cube((2, 3, 0), (2, 2, 2), resolution=1)
+    renderer = CameraImageRenderer({camera0: "red", camera1: "blue"}, show_image_frame=True, show_epipolar_lines=True)
     scene = Scene(cube, [camera0, camera1], renderer)
     scene.project(True)
     plt.waitforbuttonpress(-1)
-
--- multiple_view_geometry/camera_image_renderer.py
+++ multiple_view_geometry/camera_image_renderer.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+
 class CameraImageRenderer(object):
     def __init__(self, cameras_with_color, show_image_frame, show_epipolar_lines):
         self._show_image_frame = show_image_frame
@@ -7,20 +8,20 @@ class CameraImageRenderer(object):
         self._fig, self._axes = plt.subplots(1, len(cameras_with_color))
         self._camera_to_ax = {camera: ax for ax, camera in zip(self._axes, cameras_with_color.keys())}
         self._cameras_with_color = cameras_with_color
-        self._fig.suptitle('keypoints in image frame')
+        self._fig.suptitle("keypoints in image frame")
 
     def render_image_frame_in_camera(self, points_in_image_frame, camera):
         if self._show_image_frame:
             self._camera_to_ax[camera].set_xlim(0, camera.image_resolution[0])
             self._camera_to_ax[camera].set_ylim(0, camera.image_resolution[1])
-            self._camera_to_ax[camera].set_title('image frame in camera {}'.format(camera.name))
-            self._camera_to_ax[camera].plot(points_in_image_frame[0,:], points_in_image_frame[1,:], 'o', color=self._cameras_with_color[camera])
+            self._camera_to_ax[camera].set_title("image frame in camera {}".format(camera.name))
+            self._camera_to_ax[camera].plot(
+                points_in_image_frame[0, :], points_in_image_frame[1, :], "o", color=self._cameras_with_color[camera]
+            )
 
     def render_epipolar_line(self, line_end_points, camera, interactive=False):
         if self._show_epipolar_lines:
-            self._camera_to_ax[camera].plot(line_end_points[:,0], line_end_points[:,1])
+            self._camera_to_ax[camera].plot(line_end_points[:, 0], line_end_points[:, 1])
             if interactive:
                 plt.waitforbuttonpress(0.2)
                 plt.draw()
-
-
